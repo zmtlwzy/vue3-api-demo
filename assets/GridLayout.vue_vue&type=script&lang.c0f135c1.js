712b894e43440238e0156b053db2859edfb5a75d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-16 14:23:22 +0000

    Deploy zmtlwzy/vue3-demo to github.com/zmtlwzy/vue3-demo.git:gh-pages

---
+++ assets/GridLayout.vue_vue&type=script&lang.c0f135c1.js
@@ -0,0 +1,1 @@
+import{e as a,i as e,ac as s}from"./vendor.4226770e.js";var l=a({name:"GridLayout",props:{cols:{type:Number,default:2}},setup:(a,{slots:l})=>()=>e(s,{cols:a.cols,"x-gap":"16","y-gap":"24"},{default:()=>{var a;return[null==(a=l.default)?void 0:a.call(l)]}})});export{l as _};

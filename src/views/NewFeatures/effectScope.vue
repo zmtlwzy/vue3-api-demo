3e9780f57d0665141a3666ab8fc1eb4a8e5292d8
zmtlwzy <1025302051@qq.com>
2021-08-18 22:00:48 +0800

    feat(effectScope): add functions

--- src/views/NewFeatures/effectScope.vue
+++ src/views/NewFeatures/effectScope.vue
@@ -7,6 +7,7 @@
         <n-button @click="handleScope('on')">on</n-button>
         <n-button @click="handleScope('off')">off</n-button>
         <n-button @click="handleScope('stop')">stop</n-button>
+        <n-button @click="handleScope('all stop')">all stop</n-button>
       </n-space>
     </template>
   </n-card>
@@ -28,8 +29,13 @@
     name: 'effectScope',
     setup() {
       const counter = ref(1);
-      const scope = effectScope(true);
+      const scope = effectScope();
+      const scope2 = effectScope();
+
       console.log(scope, 'scope');
+      console.log(scope2, 'scope2');
+      console.log(scope?.parent === scope2?.parent);
+
       const state = scope.run(() => {
         const doubled = computed(() => counter.value * 2);
 
@@ -44,10 +50,22 @@
         };
       });
 
-      console.log(getCurrentScope(), getCurrentScope() === scope.parent);
-      console.log(state, 'state');
-      const handleScope = (keyName: 'on' | 'off' | 'stop') => {
+      scope2.on();
+      scope2.cleanups.push(() => {
+        console.log('scope2 cleanups push');
+      });
+      scope.cleanups.push(() => {
+        console.log('scope cleanups push');
+      });
+
+      const handleScope = (keyName: 'on' | 'off' | 'stop' | 'all stop') => {
+        const curScope = getCurrentScope();
+        console.log(curScope);
+        if (keyName === 'all stop') {
+          return console.log(curScope?.stop());
+        }
         scope[keyName]();
+        console.log(curScope === scope, curScope === scope2);
       };
 
       return {

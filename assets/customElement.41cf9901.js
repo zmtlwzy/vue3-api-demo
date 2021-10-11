98cea5c6c3a0cb057a166a8903e274db7a57c79e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-11 07:14:01 +0000

    Deploy zmtlwzy/vue3-api-demo to github.com/zmtlwzy/vue3-api-demo.git:gh-pages

---
+++ assets/customElement.41cf9901.js
@@ -0,0 +1,1 @@
+import{_ as e,F as o}from"./index.ea82313a.js";import{g as s,e as t,o as n}from"./vendor.8b730130.js";const a=s({name:"customElement",setup(){console.log(o.styles)}});function r(c,m,p,f,l,_){return n(),t("my-foo")}var u=e(a,[["render",r]]);export{u as default};
